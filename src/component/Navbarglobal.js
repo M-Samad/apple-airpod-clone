5f314b56f70de7304b3af63af1349335c64f6524
M-Samad <samadhashmi007@gmail.com>
2022-12-09 16:46:47 +0530

    First Navbar

---
+++ src/component/Navbarglobal.js
@@ -0,0 +1,74 @@
+import React from "react";
+import "./css/globalnav.css";
+
+function NavbarGlobal() {
+  return (
+    <nav className="nav-global">
+      <div className="nav-container">
+        <div className="nav-menu">
+          <li className="nav-link logo">
+            <a className="link-a" href=""></a>
+          </li>
+          <li className="nav-link">
+            <a className="link-a" href="">
+              Store
+            </a>
+          </li>
+          <li className="nav-link">
+            <a className="link-a" href="">
+              Mac
+            </a>
+          </li>
+          <li className="nav-link">
+            <a className="link-a" href="">
+              iPad
+            </a>
+          </li>
+          <li className="nav-link">
+            <a className="link-a" href="">
+              iPhone
+            </a>
+          </li>
+          <li className="nav-link">
+            <a className="link-a" href="">
+              Watch
+            </a>
+          </li>
+          <li className="nav-link">
+            <a className="link-a" href="">
+              AirPods
+            </a>
+          </li>
+          <li className="nav-link">
+            <a className="link-a" href="">
+              TV & Home
+            </a>
+          </li>
+          <li className="nav-link">
+            <a className="link-a" href="">
+              Only on Apple
+            </a>
+          </li>
+          <li className="nav-link">
+            <a className="link-a" href="">
+              Accessories
+            </a>
+          </li>
+          <li className="nav-link">
+            <a className="link-a" href="">
+              Support
+            </a>
+          </li>
+          <li className="nav-link search">
+            <a className="link-a" href=""></a>
+          </li>
+          <li className="nav-link bag">
+            <a className="link-a" href=""></a>
+          </li>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavbarGlobal;
